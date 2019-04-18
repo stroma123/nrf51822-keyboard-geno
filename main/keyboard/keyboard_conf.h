a37d556b154f75cd8c1c8b425232acea8e8f8583
Geno [格诺] <genokolar@gmail.com>
2019-04-18 20:46:06 +0800

    添加BLE60 Rec.D配置

--- main/keyboard/keyboard_conf.h
+++ main/keyboard/keyboard_conf.h
@@ -8,7 +8,7 @@
 #ifdef KEYBOARD_60
 
 
-#ifdef KEYBOARD_REVB
+#ifdef KEYBOARD_REVB  //BLE60 REVC
 
 #define KEYBOARD_ADC NRF_ADC_CONFIG_INPUT_2
 static const uint8_t column_pin_array[MATRIX_COLS] = {19,17,15,13,11,9,7,6,5,4,3,2,0,30};
@@ -37,7 +37,9 @@ static const uint8_t row_pin_array[MATRIX_ROWS] = {24,20,29,25,28};
 
 #define LED_POSITIVE
 
-#else  //BLE60 REVC
+#endif 
+ 
+#ifdef KEYBOARD_REVC  //BLE60 REVC
 
 #define KEYBOARD_ADC NRF_ADC_CONFIG_INPUT_3
 static const uint8_t column_pin_array[MATRIX_COLS] = {30,28,24,22,21,13,14,15,16,17,18,19,20,0};
@@ -49,6 +51,38 @@ static const uint8_t row_pin_array[MATRIX_ROWS] = {29,25,23,4,3};
 #define PIN_EXT2  11
 #define PIN_EXT3  12
 #define PIN_EXT4  1
+
+
+#define UART_TXD 7
+#define UART_RXD 6
+
+#define BOOTLOADER_BUTTON           9
+
+#define LED_CHARGING                PIN_EXT1
+#define LED_FULL                    PIN_EXT4
+#define LED_BLE                     PIN_EXT2
+#define LED_USR1                    PIN_EXT4
+
+#define UPDATE_IN_PROGRESS_LED      PIN_EXT4
+#define ADVERTISING_LED_PIN_NO      PIN_EXT4
+#define CONNECTED_LED_PIN_NO        PIN_EXT4
+
+#define LED_POSITIVE
+
+#endif
+
+#ifdef KEYBOARD_REVD  //BLE60 REVD
+
+#define KEYBOARD_ADC NRF_ADC_CONFIG_INPUT_3
+static const uint8_t column_pin_array[MATRIX_COLS] = {30,28,24,22,21,10,11,12,13,14,15,16,17,18};
+static const uint8_t row_pin_array[MATRIX_ROWS] = {29,25,23,4,3};
+
+#define LED_CAPS  5
+#define LED_RGB   8
+#define PIN_EXT1  19
+#define PIN_EXT2  20
+#define PIN_EXT3  1
+#define PIN_EXT4  0
 
 
 #define UART_TXD 7
