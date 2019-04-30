540d80966eba26554649cfa6c8f51d2ef1397790
Geno [格诺] <genokolar@gmail.com>
2019-04-30 10:01:48 +0800

    修正Bootloader无法升级问题

--- bootloader/project/arm5_no_packs/RTE/Device/nRF51422_xxAC/arm_startup_nrf51.s
+++ bootloader/project/arm5_no_packs/RTE/Device/nRF51422_xxAC/arm_startup_nrf51.s
@@ -34,7 +34,7 @@
                 IF :DEF: __STACK_SIZE
 Stack_Size      EQU     __STACK_SIZE
                 ELSE
-Stack_Size      EQU     255
+Stack_Size      EQU     2048
                 ENDIF
 
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
